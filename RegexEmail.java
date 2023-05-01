ce5e37848024e5395ee73b2a5c75cf27b39c5e81
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-05-01 18:36:53 +0530

    Email_validation

--- RegexEmail.java
+++ RegexEmail.java
@@ -4,9 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexEmail {
-    static Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(?:[bridgelab]+\\.)+[co]{2,}$");
+    static Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+\\.+[A-Za-z0-9+_.-]+@(?:[bridgelab]+\\.)+[co]{2,}$");
     public static void main(String[] args) {
-        String zipcode = "abc.xyz@bridegelab.co";
+        String zipcode = "abc.xyz91@bridegelab.co";
         Matcher matcher = pattern.matcher(zipcode);
         if (matcher.matches()) {
             System.out.println("Valid");
