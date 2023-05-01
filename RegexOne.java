c6c24c0876a1ca7f819dfb889eee4c9530a313f2
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-05-01 18:04:03 +0530

    POSTAL_CODE

--- RegexOne.java
+++ RegexOne.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 public class RegexOne {
     static Pattern pattern = Pattern.compile("^([0-9]{6}+$)");
     public static void main(String[] args) {
-        String zipcode = "A400088";
+        String zipcode = "400088B";
         Matcher matcher = pattern.matcher(zipcode);
         if (matcher.matches()) {
             System.out.println("Valid");
