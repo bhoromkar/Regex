733dd553d2c9317a10082a3281c5ee0d6df9e516
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-05-01 18:31:00 +0530

    Email_validation

--- RegexEmail.java
+++ RegexEmail.java
@@ -4,9 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexEmail {
-    static Pattern pattern = Pattern.compile("(^abc\\.[a-zA-Z0-9]+$)");
+    static Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+@(?:[A-Za-z0-9-])+$");
     public static void main(String[] args) {
-        String zipcode = "abc.bridegelab";
+        String zipcode = "abc.xyz@bridegelab";
         Matcher matcher = pattern.matcher(zipcode);
         if (matcher.matches()) {
             System.out.println("Valid");
